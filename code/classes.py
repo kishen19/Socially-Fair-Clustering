3a6d283774cc4ca0e1aaac84d01805802f72438b
Sruthi Gorantla <sruthigorantla@users.noreply.github.com>
2021-07-08 22:08:04 +0530

    add Dataset class

--- code/classes.py
+++ code/classes.py
@@ -51,3 +51,33 @@ class Affine:
 
     def distance(self,point):
         pass
+    
+class Dataset:
+    def __init__(self, name, n, m, ell):
+        self.name = name
+        self.n = n
+        self.m = m # no. of points
+        self.ell = ell
+        self.result = {}
+
+    
+    def add_new_result(self, algorithm, k, cost, coreset_cost, running_time, num_iters):
+        if algorithm not in self.result.keys():
+            self.result[algorithm] = {}
+
+        self.result[algorithm][k] = {'cost': cost, 
+                        'coreset_cost': coreset_cost,
+                        'running_time': running_time, 
+                        'num_iters': num_iters}
+
+    def time_per_iteration(self, algorithm, k):
+        return self.result[algorithm][k]['running_time']/self.result[algorithm][k]['num_iters']
+    
+    def cost(self, algorithm, k):
+        return self.result[algorithm][k]['cost']
+
+    def coreset_cost(self, algorithm, k):
+        return self.result[algorithm][k]['coreset_cost']    
+
+    def k_vs_val(self, algorithm, y):
+        return self.result[algorithm].keys(), [v[y] for v in self.result[algorithm].values()]    
